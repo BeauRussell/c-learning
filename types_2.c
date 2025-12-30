3bce85f762b895ffc5a1faf6dfc50f44e04fb2bf
Beau Russell <yourknightmares@gmail.com>
2025-12-30 07:10:32 -0600

    Type signing

---
+++ types_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	signed int b;
+	signed c;
+	unsigned int d;
+	unsigned e;
+
+	char ch = 'B';
+	printf("%c\n", ch); // B
+	printf("%d\n", ch); // 66
+	
+	char f;
+	signed char g;
+	unsigned char h;
+}
